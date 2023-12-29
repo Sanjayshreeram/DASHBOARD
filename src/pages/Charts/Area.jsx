b45f470805777238bcb1fb3aef4dcb764b14ca84
Your Name <sanjayshreeram77@gmail.com>
2023-12-29 12:25:49 +0530

    added chart components

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
-
+import { ChartComponent,SeriesCollectionDirective,SeriesDirective,Inject,DateTime,Legend,SplineAreaSeries } from '@syncfusion/ej2-react-charts'
+import { areaCustomSeries,areaPrimaryXAxis,areaPrimaryYAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { Header } from '../../components'
 const Area = () => {
+  const {currentmode}=useStateContext();
   return (
-    <div>Area</div>
+
+    <div>
+
+<div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl' >
+      <Header title='Inflation Rate in percentage' category="Chart"  />
+
+      </div>
+      
+
+        <ChartComponent id="line-chart" height='420px' primaryXAxis={areaPrimaryXAxis} primaryYAxis={areaPrimaryYAxis} chartArea={{border:{width:0}}} tooltip={{enable:true}} background={currentmode==='Dark' ?'#33373E':'#fff'}>
+    <Inject services={[SplineAreaSeries,DateTime,Legend]}/>
+    
+
+   <SeriesCollectionDirective>
+    {
+      areaCustomSeries.map((item,index)=>
+      
+      <SeriesDirective key={index} {...item}/>
+      )
+    }
+   </SeriesCollectionDirective>
+   </ChartComponent>
+
+
+    </div>
+    
+ 
   )
 }
 
-export default Area
+export default Area;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -5,7 +5,7 @@ import {Header} from '../../components'
 const Line = () => {
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl' >
-      <Header title='Line Chart' category="Chart"  />
+      <Header title='Inflation Rate' category="Chart"  />
       <div className='w-full'>
         <Linechart/>
 
